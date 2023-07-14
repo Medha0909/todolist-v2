9a6a63862fe7fc1c894713445cdcd49af879fed1
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2023-07-14 03:32:05 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 const server = express();
 
 server.set('view engine', 'ejs');
+server.set('views', __dirname + '/views');
 
 server.use(bodyParser.urlencoded({
   extended: true
